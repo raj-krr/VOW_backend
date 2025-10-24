c3a9078c252f8ac95b535aa624aef032168a59ff
raj-krr <mr.rajkumar3579@gmail.com>
2025-10-24 22:15:43 +0530

    changed options

--- src/constant.ts
+++ src/constant.ts
@@ -1,8 +1,10 @@
 import { CookieOptions } from "express";
 
+const isProduction = process.env.NODE_ENV === "production";
+
 export const options: CookieOptions = {
   httpOnly: true,
-  secure: true,
-  sameSite: "none",
-  maxAge: 24 * 60 * 60 * 1000,
+  secure: isProduction, 
+  sameSite: isProduction ? "none" : "lax", 
+  maxAge: 24 * 60 * 60 * 1000, 
 };
