e128a3e1cf68a097118cdee4f90765b57007b16e
Sanchit Bhardwaj <sanchitbhardwaj044@gmail.com>
2025-10-30 15:27:56 +0530

    Fix: Adjust sameSite cookie option order for production environment

--- src/constant.ts
+++ src/constant.ts
@@ -5,6 +5,6 @@ const isProduction = process.env.NODE_ENV === "production";
 export const options: CookieOptions = {
   httpOnly: true,
   secure: isProduction,
-  sameSite: isProduction ? "lax" : "none",
+  sameSite: isProduction ? "none" : "lax",
   maxAge: 24 * 60 * 60 * 1000,
 };
