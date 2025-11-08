ec69c750bf4df5d3cf051aec32a2030d67b9ea0e
raj-krr <mr.rajkumar3579@gmail.com>
2025-11-08 19:40:13 +0530

    send direct msg

--- src/controllers/directMessageController.ts
+++ src/controllers/directMessageController.ts
@@ -3,6 +3,62 @@ import DirectMessage from "../models/directMessage";
 import Workspace from "../models/workspace";
 
 
+export const sendDirectMessage = async (req: Request, res: Response) => {
+  try {
+    const { workspaceId, user1, user2 } = req.params;
+    const { content, attachments } = req.body;
+
+    
+    const currentUserId = (req as any).user?._id; 
+
+    if (!currentUserId)
+      return res.status(401).json({ error: "Unauthorized: user not found in token" });
+
+    const workspace = await Workspace.findById(workspaceId);
+    if (!workspace)
+      return res.status(404).json({ error: "Workspace not found" });
+
+    const isMember1 = workspace.members.some((m) => String(m) === user1);
+    const isMember2 = workspace.members.some((m) => String(m) === user2);
+    if (!isMember1 || !isMember2)
+      return res.status(403).json({ error: "Users not in same workspace" });
+
+    let sender, receiver;
+
+    if (String(currentUserId) === user1) {
+      sender = user1;
+      receiver = user2;
+    } else if (String(currentUserId) === user2) {
+      sender = user2;
+      receiver = user1;
+    } else {
+      return res
+        .status(403)
+        .json({ error: "You are not part of this direct chat" });
+    }
+
+    if (!content || content.trim() === "")
+      return res.status(400).json({ error: "Message content is required" });
+
+    const message = await DirectMessage.create({
+      workspaceId,
+      sender,
+      receiver,
+      content,
+      attachments,
+    });
+
+   const savedMessage = await DirectMessage.findById(message._id)
+  .populate("sender", "username avatar")
+  .populate("receiver", "username avatar");
+
+    res.status(201).json(savedMessage);
+  } catch (err: any) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
 export const getDirectMessages = async (req: Request, res: Response) => {
   try {
     const { workspaceId, user1, user2 } = req.params;
--- src/routes/directMessageRoutes.ts
+++ src/routes/directMessageRoutes.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import {
+  sendDirectMessage,
   getDirectMessages,
   deleteDirectMessage,
 } from "../controllers/directMessageController";
 
+import { verifyJWT } from "../middlewares/authmiddleware";
+
 const router = express.Router();
+
+router.post("/:workspaceId/:user1/:user2", verifyJWT,sendDirectMessage);
 
 router.get("/:workspaceId/:user1/:user2", getDirectMessages);
 
