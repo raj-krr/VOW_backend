3cd31238d5161378070c2669d5a334666c33d224
raj-krr <mr.rajkumar3579@gmail.com>
2025-11-10 23:42:47 +0530

    updated validation middleware

--- src/middlewares/validate.ts
+++ src/middlewares/validate.ts
@@ -2,11 +2,15 @@ import { Request, Response, NextFunction } from "express";
 import {ZodSchema }from "zod";
 
 export const validate =
-  (schema: ZodSchema<any>) => (req: Request, res: Response, next: NextFunction) => {
+  (schema: ZodSchema<any>) =>
+  (req: Request, res: Response, next: NextFunction) => {
     try {
-      schema.parse(req.body);
+      schema.parse({ body: req.body });
       next();
     } catch (err: any) {
-      return res.status(400).json({ success: false, msg: err.errors?.[0]?.message || "Invalid input" });
+      return res.status(400).json({
+        success: false,
+        msg: err.errors?.[0]?.message || "Invalid input",
+      });
     }
   };
