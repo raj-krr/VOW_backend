394beb76181472a6607c59a6b6bc6966485919d8
Sanchit Bhardwaj <sanchitbhardwaj044@gmail.com>
2025-11-15 04:34:31 +0530

    run cicd again due to failed res

--- src/middlewares/rateLimit.ts
+++ src/middlewares/rateLimit.ts
@@ -1,36 +1,36 @@
-// import {rateLimit} from "express-rate-limit";
-// import { Request } from "express";
+import {rateLimit} from "express-rate-limit";
+import { Request } from "express";
 
 
-// export const ipLimiter = rateLimit({
-//   windowMs: 60 * 1000, 
-//   max: 100,
-//   skip: (req) => {
-//     if (req.path && req.path.startsWith("/videochat")) return true;
-//     if (req.ip === "127.0.0.1" || req.ip === "::1") return true;
-//     return false;
-//   },
-//   message: {
-//     success: false,
-//     msg: "Too many requests from this IP. Please try again later.",
-//   },
-//   standardHeaders: true,
-//   legacyHeaders: false,
-// });
+export const ipLimiter = rateLimit({
+  windowMs: 60 * 1000, 
+  max: 100,
+  skip: (req) => {
+    if (req.path && req.path.startsWith("/videochat")) return true;
+    if (req.ip === "127.0.0.1" || req.ip === "::1") return true;
+    return false;
+  },
+  message: {
+    success: false,
+    msg: "Too many requests from this IP. Please try again later.",
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 
 
-// export const emailLimiter = rateLimit({
-//   windowMs: 2 * 60 * 1000, 
-//   max: 5,
-//   message: {
-//     success: false,
-//     msg: "Too many email requests. Try again in 2 minutes.",
-//   },
-//   keyGenerator: (req: Request) => {
-//     const email = (req.body?.email || "").toLowerCase().trim();
-//     return email;
-//   },
-//   standardHeaders: true,
-//   legacyHeaders: false,
-// });
+export const emailLimiter = rateLimit({
+  windowMs: 2 * 60 * 1000, 
+  max: 5,
+  message: {
+    success: false,
+    msg: "Too many email requests. Try again in 2 minutes.",
+  },
+  keyGenerator: (req: Request) => {
+    const email = (req.body?.email || "").toLowerCase().trim();
+    return email;
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
